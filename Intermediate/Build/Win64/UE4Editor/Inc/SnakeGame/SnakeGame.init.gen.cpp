b9ad361eafcea4ca3ff2e50c3b33a10c0c130727
Vasilii Sergutov <sergutoff.v@mail.ru>
2021-03-04 02:02:07 +0200

    Initialized Snake Elements

--- Intermediate/Build/Win64/UE4Editor/Inc/SnakeGame/SnakeGame.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/SnakeGame/SnakeGame.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodeSnakeGame_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x0E59E548,
+				0x86C9F7B3,
 				0xEBF96DA5,
 				METADATA_PARAMS(nullptr, 0)
 			};
