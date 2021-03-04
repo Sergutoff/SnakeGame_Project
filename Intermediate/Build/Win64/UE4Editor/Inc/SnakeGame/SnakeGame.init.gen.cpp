fcda08303e695f6bfdec81a03a3a657087cd4330
Vasilii Sergutov <sergutoff.v@mail.ru>
2021-03-04 02:42:17 +0200

    Initialized Snake Movement

--- Intermediate/Build/Win64/UE4Editor/Inc/SnakeGame/SnakeGame.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/SnakeGame/SnakeGame.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeSnakeGame_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x86C9F7B3,
-				0xEBF96DA5,
+				0x44A5EE7D,
+				0x421328B7,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
